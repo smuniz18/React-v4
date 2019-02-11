4088935aaceb3ccdb7e0e739df9670b2ee7a007d
Steven Muniz <steven.muniz@gmail.com>
2019-02-10 21:53:13 -0600

    Setting up a better quality layout of images with the pet info

--- src/app.js
+++ src/app.js
@@ -9,18 +9,59 @@ const petfinder = pf({
 });
 
 class App extends React.Component {
-  componentDidMount() {
-    const promise = petfinder.breed.list({ animal: "dog" });
+  constructor(props) {
+    super(props);
 
-    promise.then(console.log, console.error);
+    this.state = {
+      pets: []
+    };
+  }
+  componentDidMount() {
+    petfinder.pet
+      .find({ output: "full", location: "Austin, TX" })
+      .then(data => {
+        let pets;
+
+        if (data.petfinder.pets && data.petfinder.pets.pet) {
+          if (Array.isArray(data.petfinder.pets.pet)) {
+            pets = data.petfinder.pets.pet;
+          } else {
+            pets = [data.petfinder.pets.pet];
+          }
+        } else {
+          pets = [];
+        }
+
+        this.setState({
+          pets
+        });
+      });
   }
   render() {
     return (
       <div>
         <h1>Adopt Me!</h1>
-        <Pet name="Luna" animal="Dog" breed="Havanese" />
-        <Pet name="Peppers" animal="bird" breed="Cockatiel" />
-        <Pet name="Bella" animal="Dog" breed="Spaniel" />
+        <div>
+          {this.state.pets.map(pet => {
+            let breed;
+
+            if (Array.isArray(pet.breeds.breed)) {
+              breed = pet.breeds.breed.join(". ");
+            } else {
+              breed = pet.breeds.breed;
+            }
+            return (
+              <Pet
+                key={pet.id}
+                animal={pet.animal}
+                name={pet.name}
+                breed={breed}
+                media={pet.media}
+                location={`${pet.contact.city}, ${pet.contact.state}`}
+              />
+            );
+          })}
+        </div>
       </div>
     );
   }
