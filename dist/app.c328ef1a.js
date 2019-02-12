c7e583bc87a192364a0d50a2b0ba8b5595d981dc
Steven Muniz <steven.muniz@gmail.com>
2019-02-11 23:56:03 -0600

    Converting the carousel into an interactive click handler that will change the photo based on the image we click

--- dist/app.c328ef1a.js
+++ dist/app.c328ef1a.js
@@ -28189,12 +28189,18 @@ function (_React$Component) {
     return _possibleConstructorReturn(_this, (_temp = _this = _possibleConstructorReturn(this, (_getPrototypeOf2 = _getPrototypeOf(Carousel)).call.apply(_getPrototypeOf2, [this].concat(args))), _this.state = {
       photos: [],
       active: 0
+    }, _this.handleIndexClick = function (event) {
+      _this.setState({
+        active: +event.target.dataset.index
+      });
     }, _temp));
   }
 
   _createClass(Carousel, [{
     key: "render",
     value: function render() {
+      var _this2 = this;
+
       var _this$state = this.state,
           photos = _this$state.photos,
           active = _this$state.active;
@@ -28206,12 +28212,17 @@ function (_React$Component) {
       }), _react.default.createElement("div", {
         className: "carousel-smaller"
       }, photos.map(function (photo, index) {
-        return _react.default.createElement("img", {
-          key: photo.value,
-          src: photo.value,
-          className: index === active ? "active" : "",
-          alt: "animal thumbnail"
-        });
+        return (
+          /* eslint-disable-next-line */
+          _react.default.createElement("img", {
+            onClick: _this2.handleIndexClick,
+            key: photo.value,
+            "data-index": index,
+            src: photo.value,
+            className: index === active ? "active" : "",
+            alt: "animal thumbnail"
+          })
+        );
       })));
     }
   }], [{
@@ -28446,7 +28457,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63628" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63692" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
--- src/Carousel.js
+++ src/Carousel.js
@@ -14,6 +14,11 @@ class Carousel extends React.Component {
 
     return { photos };
   }
+  handleIndexClick = event => {
+    this.setState({
+      active: +event.target.dataset.index
+    });
+  };
   render() {
     const { photos, active } = this.state;
 
@@ -22,8 +27,11 @@ class Carousel extends React.Component {
         <img src={photos[active].value} alt="primary animal" />
         <div className="carousel-smaller">
           {photos.map((photo, index) => (
+            /* eslint-disable-next-line */
             <img
+              onClick={this.handleIndexClick}
               key={photo.value}
+              data-index={index}
               src={photo.value}
               className={index === active ? "active" : ""}
               alt="animal thumbnail"
