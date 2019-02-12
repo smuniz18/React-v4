b7bee5313bdae4265397d677d2d50550aea3c763
Steven Muniz <steven.muniz@gmail.com>
2019-02-11 23:32:29 -0600

    Building out Carousel component to be displayed on details page

--- src/Carousel.js
+++ src/Carousel.js
@@ -14,7 +14,25 @@ class Carousel extends React.Component {
 
     return { photos };
   }
-  render() {}
+  render() {
+    const { photos, active } = this.state;
+
+    return (
+      <div className="carousel">
+        <img src={photos[active].value} alt="primary animal" />
+        <div className="carousel-smaller">
+          {photos.map((photo, index) => (
+            <img
+              key={photo.value}
+              src={photo.value}
+              className={index === active ? "active" : ""}
+              alt="animal thumbnail"
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Carousel;
--- src/Details.js
+++ src/Details.js
@@ -1,5 +1,6 @@
 import React from "react";
 import pf from "petfinder-client";
+import Carousel from "./Carousel";
 
 const petfinder = pf({
   key: process.env.API_KEY,
@@ -41,10 +42,11 @@ class Details extends React.Component {
       return <h1>loading … </h1>;
     }
 
-    const { animal, breed, location, description } = this.state;
+    const { animal, breed, location, description, media } = this.state;
 
     return (
       <div className="details">
+        <Carousel media={media} />
         <div>
           <h1>{name}</h1>
           <h2>{`${animal} — ${breed} — ${location}`}</h2>
