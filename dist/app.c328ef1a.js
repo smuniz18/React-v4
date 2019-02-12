d9b8ba960b0185360f40b6fbf862f828fed4d850
Steven Muniz <steven.muniz@gmail.com>
2019-02-12 01:22:14 -0600

    Adding breed selection so user can pick a specific breed after the animal type has been selected

--- dist/app.c328ef1a.js
+++ dist/app.c328ef1a.js
@@ -28379,7 +28379,9 @@ exports.default = void 0;
 
 var _react = _interopRequireDefault(require("react"));
 
-var _petfinderClient = require("petfinder-client");
+var _petfinderClient = _interopRequireWildcard(require("petfinder-client"));
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {}; if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj.default = obj; return newObj; } }
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
@@ -28400,6 +28402,11 @@ function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.g
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
 
 function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+var petfinder = (0, _petfinderClient.default)({
+  key: "8751bed9f1e6f37e0cad9f6c3c644ed8",
+  secret: "3c61c1673c0dcc05cc0abe7441c719a6"
+});
 
 var SearchParams =
 /*#__PURE__*/
@@ -28422,19 +28429,50 @@ function (_React$Component) {
     return _possibleConstructorReturn(_this, (_temp = _this = _possibleConstructorReturn(this, (_getPrototypeOf2 = _getPrototypeOf(SearchParams)).call.apply(_getPrototypeOf2, [this].concat(args))), _this.state = {
       location: "Austin, TX",
       animal: "",
-      breed: ""
+      breed: "",
+      breeds: []
     }, _this.handleLocationChange = function (event) {
       _this.setState({
         location: event.target.value
       });
     }, _this.handleAnimalChange = function (event) {
       _this.setState({
-        animal: event.target.value
+        animal: event.target.value,
+        breed: ""
+      }, _this.getBreeds);
+    }, _this.handleBreedChange = function (event) {
+      _this.setState({
+        breed: event.target.value
       });
     }, _temp));
   }
 
   _createClass(SearchParams, [{
+    key: "getBreeds",
+    value: function getBreeds() {
+      var _this2 = this;
+
+      if (this.state.animal) {
+        petfinder.breed.list({
+          animal: this.state.animal
+        }).then(function (data) {
+          if (data.petfinder && data.petfinder.breeds && Array.isArray(data.petfinder.breeds.breed)) {
+            _this2.setState({
+              breeds: data.petfinder.breeds.breed
+            });
+          } else {
+            _this2.setState({
+              breeds: []
+            });
+          }
+        });
+      } else {
+        this.setState({
+          breeds: []
+        });
+      }
+    }
+  }, {
     key: "render",
     value: function render() {
       return _react.default.createElement("div", {
@@ -28458,7 +28496,20 @@ function (_React$Component) {
           key: animal,
           value: animal
         }, animal);
-      }))));
+      }))), _react.default.createElement("label", {
+        htmlFor: "breed"
+      }, "Breed", _react.default.createElement("select", {
+        id: "breed",
+        value: this.state.breed,
+        onChange: this.handleBreedChange,
+        onBlur: this.handleBreedChange,
+        disabled: this.state.breeds.length === 0
+      }, _react.default.createElement("option", null), this.state.breeds.map(function (breed) {
+        return _react.default.createElement("option", {
+          key: breed,
+          value: breed
+        }, breed);
+      }))), _react.default.createElement("button", null, "Submit"));
     }
   }]);
 
@@ -28559,7 +28610,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64105" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59980" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
